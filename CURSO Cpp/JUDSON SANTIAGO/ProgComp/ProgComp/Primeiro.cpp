1ba95e2a54b38b460fd7bfa321734df96716bd3a
JuanGabriel <jgstudent7@hotmail.com>
2024-09-29 00:20:26 +0200

    Començando o curdo de c++ do judson

---
+++ CURSO Cpp/JUDSON SANTIAGO/ProgComp/ProgComp/Primeiro.cpp
@@ -0,0 +1,16 @@
+// primeiro.cpp - mostra uma mensagem
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "bem vindo ao C++";
+	coux << endl;
+	coup << "Meu primeiro programa!" << endl;
+
+
+
+
+	return 0;
+}
